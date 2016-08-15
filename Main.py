f7030c4e57e6e6a9355250cfb2914e42aeeb10f6
AakashKT <aku2162@gmail.com>
2016-08-15 21:17:53 +0530

    Screen class

---
+++ Main.py
@@ -0,0 +1,7 @@
+from screen import Screen
+
+tp = Screen(30, 32);
+
+tp.drawBounds();
+tp.drawObject("*", 4, 1);
+tp.updateScreen();
---
+++ screen.py
@@ -0,0 +1,48 @@
+
+class Screen:
+
+	def __init__(self, width, height):
+		self.width = width;
+		self.height = height;
+
+		self.displayMatrix = [];
+		self.hasBounds = False;
+
+
+	def updateScreen(self):
+		for i in range(self.height):
+			for j in range(self.width):
+				print(self.displayMatrix[i][j], end=' ');
+
+			print("");
+
+
+	def drawBounds(self):
+		self.height = self.height+2;
+		self.width = self.width+2;
+
+		self.hasBounds = True;
+
+		for i in range(self.height):
+			tempMatrix = [];
+
+			if(i==0 or i==self.height-1):
+				for j in range(self.width):
+					tempMatrix.append("-");
+			else:
+				for j in range(self.width):
+					if(j==0 or j==self.width-1):
+						tempMatrix.append("|");
+					else:
+						tempMatrix.append(" ");
+
+			self.displayMatrix.append(tempMatrix);
+
+
+	def drawObject(self, obj, x, y):
+		if(self.hasBounds==False):
+			self.displayMatrix[y-1][x-1] = obj;
+		else:
+			self.displayMatrix[y][x] = obj;
+
+
